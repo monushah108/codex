c1b9adb138a1460b02a1849e61e6e7c801255448
monushah108 <sonuk627422@gmail.com>
2026-02-21 18:15:50 +0530

    auth updated

--- lib/auth-action.ts
+++ lib/auth-action.ts
@@ -1,13 +1,37 @@
 import { createAuthClient } from "better-auth/client";
+import { headers } from "next/headers";
 const authClient = createAuthClient();
 
 export const socialAuth = async (provider: string) => {
   const data = await authClient.signIn.social({
-    provider: "google",
+    provider,
     callbackURL: "/",
   });
 };
 
+export const socialSignOut = async (provider: string) => {
+  return await authClient.signOut({
+    provider,
+    headers: headers(),
+  });
+};
+
 export const emailAuth = async (email: string, password: string) => {
-  const data = await authClient.signUp.email({ email, password });
+  return await authClient.signUp.email({
+    email,
+    password,
+  });
+};
+
+export const getSession = async () => {
+  const session = await authClient.getSession({
+    headers: headers(),
+  });
+  return session;
+};
+
+export const signOut = async () => {
+  await authClient.signOut({
+    headers: headers(),
+  });
 };
--- lib/auth.ts
+++ lib/auth.ts
@@ -7,10 +7,10 @@ const client = new MongoClient(process.env.MONGODB_URI as string);
 const db = client.db();
 
 export const auth = betterAuth({
-  database: mongodbAdapter(db, {
-    client,
-  }),
-  // baseURL: process.env.BETTER_AUTH_URL,
+  // database: mongodbAdapter(db, {
+  //   client,
+  // }),
+  baseURL: process.env.BETTER_AUTH_URL,
   emailAndPassword: {
     enabled: true,
   },
@@ -18,6 +18,10 @@ export const auth = betterAuth({
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     },
   },
 
