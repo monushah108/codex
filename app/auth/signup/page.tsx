6f43664b4a22c6f935b276fe97d14e30b2ff0c08
monushah108 <sonuk627422@gmail.com>
2026-02-25 16:05:23 +0530

    ui updated

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,23 +1,18 @@
-import { Binary } from "lucide-react";
+import { GalleryVerticalEnd } from "lucide-react";
 
-import { LoginForm } from "@/components/login-form";
-import { Metadata } from "next";
+import { SignupForm } from "@/components/signup-form";
 
-export const metadata: Metadata = {
-  title: "login",
-};
-
-export default function LoginPage() {
+export default function SignupPage() {
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
         <a href="#" className="flex items-center gap-2 self-center font-medium">
           <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-            <Binary className="size-4" />
+            <GalleryVerticalEnd className="size-4" />
           </div>
-          codex .
+          codex Inc.
         </a>
-        <LoginForm />
+        <SignupForm />
       </div>
     </div>
   );
