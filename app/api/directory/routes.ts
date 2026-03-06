bd28f9f24bfb13d162c751947d6a68bbce5cdd00
monushah108 <sonuk627422@gmail.com>
2026-03-06 13:29:32 +0530

    playground update

--- app/api/directory/routes.ts
+++ app/api/directory/routes.ts
@@ -1,6 +1,7 @@
 import Directory from "@/model/directory";
+import { NextRequest } from "next/server";
 
-export async function GET(request: any) {
+export async function GET(request: NextRequest) {
   const userId = request.userId;
 
   const dir = await Directory.findById(userId.rootDirId);
--- app/playground/[roomId]/page.tsx
+++ app/playground/[roomId]/page.tsx
@@ -1,45 +1,36 @@
-// "use client";
-// import { lazy, useEffect } from "react";
 import {
   ResizableHandle,
   ResizablePanelGroup,
 } from "@/components/ui/resizable";
 
-// import StatusBar from "@/components/editor/StatusBar";
-// const CodeWindow = lazy(() => import("@/components/editor/CodeWindow"));
-// const FileExplore = lazy(() => import("@/components/editor/FileExplore"));
-// const ChatBox = lazy(() => import("@/components/editor/ChatBox"));
-// import PlayHeader from "@/components/editor/playHeader";
-// import { useParams, useRouter } from "next/navigation";
 import StatusBar from "@/components/editor/StatusBar";
 import PlayHeader from "@/components/editor/playHeader";
 
 import CodeWindow from "@/components/editor/CodeWindow";
 import FileExplore from "@/components/editor/FileExplore";
 import ChatBox from "@/components/editor/ChatBox";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
 
 export default async function Page({ params }) {
   const { roomId } = await params;
-  // const router = useRouter();
+  const cookieStore = await cookies();
 
-  // const IsvalidUser = async () => {
-  //   try {
-  //     const res = await fetch(`/api/playground/${roomId}`, {
-  //       credentials: "include",
-  //     });
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/playground/${roomId}`,
+    {
+      headers: {
+        Cookie: cookieStore.toString(),
+      },
+      cache: "no-store",
+    },
+  );
 
-  //     if (res.status == 403) {
-  //       router.push(`/playground/join/${roomId}`);
-  //     } else if (res.status == 400) {
-  //     }
-  //   } catch {
-  //     router.push("/");
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   IsvalidUser();
-  // }, []);
+  if (res.status == 403) {
+    redirect(`/playground/join/${roomId}`);
+  } else if (res.status == 400) {
+    redirect("/profile");
+  }
 
   return (
     <div className=" flex flex-col min-h-svh  bg-[#1e1e1e] text-[#d4d4d4] overflow-hidden">
